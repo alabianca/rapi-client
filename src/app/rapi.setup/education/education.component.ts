2e367fe1891dc3d8561ded9ae31a0d33ff954f22
Alexander <labianca.alex@gmail.com>
2019-06-01 14:34:27 -0500

    education

--- src/app/rapi.setup/education/education.component.ts
+++ src/app/rapi.setup/education/education.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 
 @Component({
@@ -24,6 +24,9 @@ export class EducationComponent implements OnInit {
     return new FormGroup({
       school: new FormControl(null),
       degree: new FormControl(null),
+      startDate: new FormControl(null),
+      endDate: new FormControl(null),
+      gpa: new FormControl(0.0, [Validators.pattern('[0-9]*\.[0-9]*')])
     })
   }
 
