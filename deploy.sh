f5619b1dcb5464867542892d9598654d1c60c4bf
Alexander <labianca.alex@gmail.com>
2019-07-18 23:00:31 -0500

    heroku deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-#docker login --username=_ --password=$HEROKU_TOKEN registry.heroku.com
+sed -e 's/$HEROKU_USERNAME/'"$HEROKU_USERNAME"'/g' -e 's/$HEROKU_TOKEN/'"$HEROKU_TOKEN"'/g' heroku_auth > ~/.netrc
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login --username=_ --password="$HEROKU_TOKEN" registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
